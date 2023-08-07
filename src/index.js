ddde64b3ef566a049cfb785f01fa0333eabfd17a
AndreImasato <andresimasato@gmail.com>
2023-08-06 22:12:27 -0300

    Changes instantiation

--- src/index.js
+++ src/index.js
@@ -8,51 +8,52 @@ import bodyParser from 'body-parser';
 import models, { connectDb } from './models';
 import routes from './routes';
 
-// multer instance
-const upload = multer();
-
-// Create instance of express application
-const app = express();
-
-// Add parser middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Parsing multipart/form-data
-app.use(upload.array());
-app.use(express.static('public'));
-
-// Use CORS
-app.use(cors());
-
-// Implements Helmet for extra security
-app.use(helmet());
-
-// Modular routes
-/**
- * app.use('/example', routes.example);
- */
-
-app.get(
-  '/',
-  (req, res) => {
-    return res.status(200).send('API Live!')
-  }
-);
-
-// Middleware for errors
-app.use((error, req, res) => {
-  if (!error.statusCode) error.statusCode = 500;
-
-  
-  return res
-    .status(error.statusCode)
-    .json({ error: error.toString() });
-});
-
 // Start express server
 connectDb()
   .then(async () => {
+
+    // multer instance
+    const upload = multer();
+
+    // Create instance of express application
+    const app = express();
+
+    // Add parser middlewares
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    // Parsing multipart/form-data
+    app.use(upload.array());
+    app.use(express.static('public'));
+
+    // Use CORS
+    app.use(cors());
+
+    // Implements Helmet for extra security
+    app.use(helmet());
+
+    // Modular routes
+    /**
+     * app.use('/example', routes.example);
+     */
+
+    app.get(
+      '/',
+      (req, res) => {
+        return res.status(200).send('API Live!')
+      }
+    );
+
+    // Middleware for errors
+    app.use((error, req, res) => {
+      if (!error.statusCode) error.statusCode = 500;
+
+      
+      return res
+        .status(error.statusCode)
+        .json({ error: error.toString() });
+    });
+
     app.listen(process.env.PORT,  () => {
       console.log(`Example app listening on port ${process.env.PORT}`);
     })
